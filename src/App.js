dcc0e3a66e680c7b3f2dd3428cc7ade2050fcdb2
loamysand <laneeboyd@gmail.com>
2026-02-18 12:33:22 -0600

    configure for gh pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import React, {Component} from "react";
 import Main from './components/MainComponent';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 class App extends Component{
 render(){
   return(
-      <BrowserRouter>
+      <HashRouter>
         <div className="app-shell">
           <Main/>
         </div>
-      </BrowserRouter>
+      </HashRouter>
   )
 }
 }
